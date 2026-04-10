5adac3ece578d0d115f4c9a8a1fd15d22971f008
Jman5612 <supereasyman89@gmail.com>
2026-04-09 19:39:19 -0700

    commit all other stuff

--- src/top.sv
+++ src/top.sv
@@ -1,8 +1,29 @@
-module top (
-    /** Input Ports */
-    /** Output Ports */
+`include "src/adder.sv"
+`include "src/decoder.sv"
+
+module top(
+input wire [3:0] val1,
+input wire [3:0] val2,
+
+output logic [7:0] seg7
+
 );
 
-/** Logic */
+wire[4:0] res;
+
+
+adder adder (
+    .val1(val1),
+    .val2(val2),
+    .res(res)
+
+);
+
+decoder decoder (
+    .bcd(res[3:0]),
+    .seg7(seg7[6:0])
+);
+
+assign seg7[7] = res[4];
 
 endmodule
